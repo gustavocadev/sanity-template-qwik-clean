2fb07507864701c9d44c3b14f34d4cb71ef68f73
gustavocadev <gustavocadev@gmail.com>
2023-08-07 18:16:21 -0500

    chore: render post

---
+++ app/src/routes/post/[slug]/index.tsx
@@ -0,0 +1,37 @@
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { QPortableText } from '~/integrations/react/portableText';
+import { getPost } from '~/lib/sanity.queries';
+import { urlFor } from '~/lib/sanity.image';
+
+export const usePost = routeLoader$(({ params }) => {
+  const slug = params.slug;
+  const post = getPost(slug);
+
+  return post;
+});
+
+export default component$(() => {
+  const post = usePost();
+  return (
+    <section>
+      <figure>
+        <img
+          src={urlFor(post.value.mainImage!).width(500).height(300).url()}
+          alt={`Cover image for ${post.value.title}`}
+          width={500}
+          height={300}
+          class="rounded"
+        />
+      </figure>
+      <article>
+        <h1 class="text-xl font-semibold">{post.value.title}</h1>
+        <p>{post.value.excerpt}</p>
+        <p>{post.value._createdAt}</p>
+        <div class="text-xl">
+          <QPortableText value={post.value.body} />
+        </div>
+      </article>
+    </section>
+  );
+});
